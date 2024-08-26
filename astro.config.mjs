13e529353503f976fd36f0a74ed0eb9c149a5b8f
04AJ <alanjcd2004@gmail.com>
2024-08-26 10:09:05 -0500

    optimize web crawling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   output: "server",
   adapter: cloudflare(),
   image: {
