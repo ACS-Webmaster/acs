7c74756c8de6d72cb3bf08bc8bfc06c4f4ffe6a5
04AJ <alanjcd2004@gmail.com>
2025-09-01 19:56:57 -0500

    updated tutor hours

--- src/components/Tutors.tsx
+++ src/components/Tutors.tsx
@@ -42,22 +42,22 @@ const Tutors = () => {
             {active === 0 ? 
                 <div>
                     <br/>
-                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSwaOzeiXP2v06JWkpLingFQPP20aFmMWN_Dtl6SRYzcUXPmTsyE57itfkqIfO_NKLGBk4KKyQXe3ir/pubhtml?widget=true&amp;headers=false"></iframe>                    <br />
-                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTSBimAaPb4iBun23Oi6Wcf_3NdH8tqwdNfJ7UtFwV1C3gzgdhZNZM0EpOOXHgQ5lb652qRU_d4N5ND/pubhtml?widget=true&amp;headers=false"></iframe>                </div>
+                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQQdPb_By7iGq3oOBe2uU7Hcsn6v0EkZMaIr00pWEd0EDTp3N6lDeiU2ZVdrE2M_kyuAJjZkbTQxnCR/pubhtml?gid=1605407668&amp;single=true&amp;widget=true&amp;headers=false"></iframe>                    <br />
+                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsi7rJsI4YEl39_iC-oj5UU3Xb-6yV-FIbYn35Leuds0zkrQz7u1AzOxhtvl4YB5cCQKkU9oVKUOR7/pubhtml?gid=1311330992&amp;single=true&amp;widget=true&amp;headers=false"></iframe>             </div>
 
             : null}
             {active === 1 ? 
                 <div>
-                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRP2Eg4cqeupAPZ9N2kxha2YAn7Et2IW1ZQbMfGTFpJhF3yO82D8PnCVSbUZJuDH6Y_en46DoASVYnj/pubhtml?widget=true&amp;headers=false"></iframe>                    <br/>
+                   <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRiMOD0rFAJz0ZWqdRV6MDlTirKIYmvw5B2OQS36WszynECPAScnQkFPdtMoubQwgLLT0Yu8JeD-ssh/pubhtml?gid=2066533032&amp;single=true&amp;widget=true&amp;headers=false"></iframe>   <br/>
                 </div>
 
             : null}
             {active === 2 ? 
                 <div className="tab-pane fade" id="orgchem" role="tabpanel" aria-labelledby="orgchem-tab">
                     <br />
-                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFBy0IQcuRUp8xtgCKNrqzLqBMz4MVDtxES8cNrl9pL1FsPn1XTKJfzZ626_5ZL0R4ApI7t_RsYW4R/pubhtml?widget=true&amp;headers=false"></iframe>                    <br />
+                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSAyGT5J10N-9VLdeDOjQiw0s_Nx01_BgSAi64NQZ_3M9gXQ4rEd5bUlgLolQKw3sMyw1tuwkI0Q2Ro/pubhtml?gid=1420990976&amp;single=true&amp;widget=true&amp;headers=false"></iframe>  <br />
                     <br/>
-                    <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTz2-VOIvavSCUInKjQibtAZaQdpvmYbKaTmf8SIVoDRRV5_9Y6e5A5Fr65gqipLJrqm8w_DfHnTdZk/pubhtml?widget=true&amp;headers=false"></iframe>                </div>
+                   <iframe width="100%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQNhtvRiLhIBeWRSoG0yAfI_FdEP1LzMc5YsZCpO9a5ZBYHaiCWzQ5d54MMWXC06_aJMgHt8SFDc0DJ/pubhtml?gid=688810819&amp;single=true&amp;widget=true&amp;headers=false"></iframe>  </div>
             : null}
       
             
